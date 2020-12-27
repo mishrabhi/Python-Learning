0afacdb9545a85567b402a639b3b953b310271d0
unknown <asmyselfabhishek00@gmail.com>
2020-12-27 12:43:28 -0800

    added escape sequence as normal text

--- escapeSequence.py
+++ escapeSequence.py
@@ -12,3 +12,16 @@ print("name\tharshit")
 #If you want to print backslash(\) then you have to use double blackslash(\\).
 print("This is backslash \\")
 print("This is double blackslash \\\\")
+
+#Escape Sequence as a Normal Text:
+#output:line A \n Line B
+print("Line A \\n Line B")
+
+#output:line A \t line B
+print("line A \\t line B")
+
+#output:this is 4 backslash \\\\
+print('this is 4 backslash \\\\\\\\')
+
+#output: \" \'
+print(" \\\" \\\' ")
