b5035867ffabc5464d1ef645dd23db18e03f53a8
unknown <asmyselfabhishek00@gmail.com>
2020-12-27 12:29:22 -0800

    added escape sequences

---
+++ escapeSequence.py
@@ -0,0 +1,14 @@
+#Since we cannot use "double quotes" inside "double quotes" so now we can use it by applying backslash.
+print("hello \"world\" World")
+print('I\'m Iron Man')
+
+#We can use \n to form a new line.
+print("line A\nline B")
+print('line A\nline B\nline C')
+
+#We can use \t for tab(creates a tab space)
+print("name\tharshit")
+
+#If you want to print backslash(\) then you have to use double blackslash(\\).
+print("This is backslash \\")
+print("This is double blackslash \\\\")
