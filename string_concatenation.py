f45c76189692f8575feed5257612ec3c4b20c16c
unknown <asmyselfabhishek00@gmail.com>
2021-01-08 22:35:06 -0800

    added string concatenation

---
+++ string_concatenation.py
@@ -0,0 +1,13 @@
+#strings
+#collection of characters inside single quotes or double quotes
+
+first_name = "Abhishek"
+last_name = "Mishra"
+full_name = first_name + " " + last_name
+print(full_name)
+#print(first_name + 3) =>error
+#print(first_name + "3") => no error
+#print(first_name + str(3)) => no error
+
+#You cannot add a number into string but you can multiply numbers into strings.
+print(first_name * 3)
