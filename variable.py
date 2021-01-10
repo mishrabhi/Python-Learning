35e6f3bf2bb843b662983e74b30c3fbec193bbd0
unknown <asmyselfabhishek00@gmail.com>
2021-01-09 18:34:42 -0800

    added some variables

--- variable.py
+++ variable.py
@@ -21,3 +21,10 @@ print(user_one_name)
 #you can use camel casing also.But people mostly use camel using in writing java.
 userOneName = "Abhishek"
 print(userOneName) 
+
+#=> You can assign multiple variables in a single line 
+name, age = "Abhishek", "24"
+print("hello my name is " + name + " and my age is " + age)
+
+x=y=z=1
+print(x+y+z)
