d152f5926fc9618bd5d7696998a9ceaed8116dbd
unknown <asmyselfabhishek00@gmail.com>
2021-01-12 13:09:24 -0800

    ADD files

--- user_input.py
+++
@@ -1,8 +0,0 @@
-#user input
-#input function
-name = input("enter your name ")
-print("hello " + name)
-age = input("enter your age ")
-print("hello " + name + " " + "your age is " + age)
-marriage = input("are you marrried? ")
-print("hello " + name + " " + "your age is " + age + " " + "and you are " + marriage + " " + "married")
