898de18fabb645d8411403fe39ef5818def279ea
unknown <asmyselfabhishek00@gmail.com>
2021-01-15 20:42:12 -0800

    added indexing of string

---
+++ string_indexing.py
@@ -0,0 +1,26 @@
+#string indexing
+language = "python"
+
+# positions(index number)
+
+# p = 0  
+# y = 1
+# t = 2
+# h = 3
+# o = 4
+# n = 5
+
+print(language[2]) #//t #we use square brackets to print using indexing.
+print(language[4]) #//o
+
+#If we try to do indexing backwards,then:
+
+# p = -6
+# y = -5
+# t = -4
+# h = -3
+# o = -2
+# n = -1
+
+print(language[-5]) #//y
+print(language[-3])  #//h
