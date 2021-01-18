bc8ebea746745b00eca06bd0a4869cfb782aa9cc
unknown <asmyselfabhishek00@gmail.com>
2021-01-18 11:02:25 -0800

    added exercise on argument slicing

---
+++ Exercise/argument_slicing.py
@@ -0,0 +1,5 @@
+#Ask user name and print back username in reverse order.
+#Note: try to make your programme in 2 lines using string formatting.
+
+username = "Abhishek"
+print(username[7::-1])  #//kehsibhA
