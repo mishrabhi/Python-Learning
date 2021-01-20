1539c244d3d3d46e289f4af6156a02d20d4243f4
unknown <asmyselfabhishek00@gmail.com>
2021-01-19 22:35:30 -0800

    added if statement

---
+++ if_statement.py
@@ -0,0 +1,9 @@
+#syntax:
+#if condition:
+             #code #if condition is true then this code will execute.
+             #code
+
+
+age = int(input("enter your age"))
+if age >= 14:
+    print("you are above 14")
