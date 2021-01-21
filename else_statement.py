d99153199c42ad029d43cdb84c289bcc40ab48fa
unknown <asmyselfabhishek00@gmail.com>
2021-01-21 10:21:11 -0800

    added else statement

---
+++ else_statement.py
@@ -0,0 +1,8 @@
+# Else Statement:
+# We always use else statement after if statement.
+
+age = int(input("enter your age"))
+if age>=15:
+    print("you are above 15")
+else:
+    print("sorry!you cannot open this.")
