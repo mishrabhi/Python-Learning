e9df6cadbff1e52547a4eea31cb45db3655bb265
unknown <asmyselfabhishek00@gmail.com>
2021-01-29 22:12:16 -0800

    added while loop

---
+++ while_loop.py
@@ -0,0 +1,18 @@
+# While Loop
+#print("hello World")  #10 times
+i = 0
+while i <= 10:
+    print("hello world")
+    i = i + 1
+
+    
+
+#Now we will add 1 to 10 by using While Loop:
+#sum : 1 to 10
+
+total = 0
+i = 1
+while i <= 10:
+    total += i  #(total = total + i)
+    i += 1  # (i = i + 1)
+print(total)   #//55
