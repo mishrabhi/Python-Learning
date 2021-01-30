861274e353ad0566b799c922e6eed288817a8530
unknown <asmyselfabhishek00@gmail.com>
2021-01-29 21:37:11 -0800

    added check empty or not

--- check_empty_orNot.py
+++ check_empty_orNot.py
@@ -6,4 +6,16 @@ if name: #true if string is not empty
     print("string is not empty")
 else:
     print("string is empty")
+#It gives string is not empty.
+
+#But when we do this:
+
+user = ""
+if user: 
+    print("string is not empty")
+else:
+    print("string is empty")
+#now it gives string is empty
+
+
 
