8d54a709f464ec3aef2e17b1fedb46e4afffc5a7
unknown <asmyselfabhishek00@gmail.com>
2021-02-02 22:13:19 -0800

    added for loop

---
+++ for-loop.py
@@ -0,0 +1,9 @@
+# For-Loop
+# for loop with range function
+# We are going to print "Hello World" 10 times using for loop range function
+
+for i in range(10):         # You can give range in range function
+    print("Hello World")    # It prints Hello World 10 times
+
+for i in range(1, 11):
+    print(f"Hello World : {i}")      # It prints Hello World : 1 to Hello World : 10 
