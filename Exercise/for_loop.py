0247f661a8895a9e7f74e16be328ce829d1f8ec2
unknown <asmyselfabhishek00@gmail.com>
2021-02-04 22:15:01 -0800

    added problem on for loop

--- Exercise/for_loop.py
+++ Exercise/for_loop.py
@@ -6,10 +6,21 @@ for i in range(1,11):
     total += i
 print(total)  #//55
 
+
 # Ask user a number and then print the sum from 1 to that number:
 
 number = int(input("enter a number: "))
 total = 0
 for i in range(1,number+1):
     total += i
+print(total)
+
+
+# ask user a number like 1256
+# calculate the sum of digits like 1 + 2 + 5 + 6
+
+total = 0
+num = input("enter a number: ")
+for i in range(0, len(num)):
+    total += int(num[i])
 print(total)
