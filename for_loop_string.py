46b3f38a068b13f087f649d3ad77bd1f483d8130
unknown <asmyselfabhishek00@gmail.com>
2021-02-19 13:52:50 -0800

    added for loop string in pyton

---
+++ for_loop_string.py
@@ -0,0 +1,19 @@
+# for_;oop_string:
+
+# Method used in all programming languages:
+name = "Abhishek"
+for i in range(len(name)):
+    print(name[i])  
+
+# Method used in python for the same:
+name = "Abhishek"
+for i in name:     # you can write this too: for i in "Abhishek":
+    print(i) 
+
+
+# 1256 ---> 1+2+5+6
+num = input("enter a number : ")
+total = 0
+for i in num:
+    total += int(i)
+print(total)  #// 9
