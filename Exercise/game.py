68dca5e17e6f07b11f78dc2d65e8997e87ba70b0
unknown <asmyselfabhishek00@gmail.com>
2021-02-19 11:38:05 -0800

    added guessing game

---
+++ Exercise/game.py
@@ -0,0 +1,27 @@
+# MODIFY NUMBER GUESSING GAME:
+
+# you have to guess a number between 1 to 100 
+# If you have guessed lower than the winning number then it prints too low
+# If you have guessed higher than the winning number then it prints too high.
+# It give you unlimited attempts to guess the winning number.
+# It also prints in how many times you have guessed the winning number correctly.
+
+winning_num = 56
+guess = 1
+number = int(input("guess a number between 1 and 100 : "))
+game_over = False
+
+while not game_over:
+    if number == winning_num:
+        print(f"you win, and you have guessed this number in {guess} times")
+        game_over = True
+    else:
+        if number < winning_num:
+            print("too low")
+            guess += 1
+            number = int(input("guess again : "))
+        else:
+            print("too high")
+            guess += 1
+            number = int(input("guess again : "))  
+    
--- break_continue.py
+++ break_continue.py
@@ -1,10 +1,10 @@
 # break and continue keyword
 # 1 to 10 print
 
-# for i in range(1, 11):
-#     if i == 8:
-#         break
-#     print(i)
+for i in range(1, 11):
+    if i == 8:
+        break
+    print(i)
 # It will print 1 to 7 and then loop breaks.
 
 #continue:
