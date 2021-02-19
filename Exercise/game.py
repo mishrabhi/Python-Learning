035502299ff147391efb277aeba59cdff30e11fc
unknown <asmyselfabhishek00@gmail.com>
2021-02-19 13:38:24 -0800

    added step arguments

--- Exercise/game.py
+++ Exercise/game.py
@@ -5,29 +5,35 @@
 # If you have guessed higher than the winning number then it prints too high.
 # It give you unlimited attempts to guess the winning number.
 # It also prints in how many times you have guessed the winning number correctly.
+# It also selects random numbers when you had guessed winning num and trying next time.
 
-# winning_num = 56
-# guess = 1
-# number = int(input("guess a number between 1 and 100 : "))
-# game_over = False
+import random
+winning_num = random randint(1,100)
+guess = 1
+number = int(input("guess a number between 1 and 100 : "))
+game_over = False
 
-# while not game_over:
-#     if number == winning_num:
-#         print(f"you win, and you have guessed this number in {guess} times")
-#         game_over = True
-#     else:
-#         if number < winning_num:
-#             print("too low")
-#             guess += 1
-#             number = int(input("guess again : "))
-#         else:
-#             print("too high")
-#             guess += 1
-#             number = int(input("guess again : "))  
+while not game_over:
+    if number == winning_num:
+        print(f"you win, and you have guessed this number in {guess} times")
+        game_over = True
+    else:
+        if number < winning_num:
+            print("too low")
+            guess += 1
+            number = int(input("guess again : "))
+        else:
+            print("too high")
+            guess += 1
+            number = int(input("guess again : "))  
+
+
+
 
 # DRY (Don't Repeat Yourself) Method:
 
-winning_num = 56
+import random
+winning_num = random.randint(1,100)
 guess = 1
 number = int(input("guess a number between 1 and 100 : "))
 game_over = False
---
+++ step.py
@@ -0,0 +1,13 @@
+# Step Argument:
+# for i in range(start argument,end argument,step)
+# for i in range(1,11):
+#     print(i)
+
+for i in range(1,11,2):   # this gives you 1-1 gap between numbers or you can say it prints odd numbers between 1 to 10.
+    print(i)
+
+for i in range(0,11,2):  # It gives you even numbers between o to 10
+    print(i)
+
+
+# Basically we use step arguments to give steps or gap between what we are printing.
