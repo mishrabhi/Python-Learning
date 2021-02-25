c81163c3ca6486dbf95a275a79911342c85f637b
unknown <asmyselfabhishek00@gmail.com>
2021-02-25 10:18:03 -0800

    added exercise on function

--- Exercise/func_practice.py
+++ Exercise/func_practice.py
@@ -4,7 +4,7 @@
 def last_char(name):    #(name)>>>is called parameters
     return name[-1]
 
-print(last_char("Abhishek"))  #//k  #("Abhishek")>>>>is called arguments
+print(last_char("Abhishek"))  #//k  #("Abhishek")>>>>is called arguments.
 print(last_char("Abhishek Mishra"))  #//a
 
 # Now we are making a function which returns the number is even or odd.
@@ -16,7 +16,7 @@ def odd_even(num):
 print(odd_even(10))  #//even
 print(odd_even(11))  #//odd
 
-# Now we are making a function which will return true when the number is even else it returns false.
+# Now we are creating a function which will return true when the number is even else it returns false.
 
 def is_even(num):
     if num%2 == 0:
@@ -24,3 +24,20 @@ def is_even(num):
     return False
 print(is_even(10)) #//True
 print(is_even(7))  #//False
+
+
+# take 2 numbers as input
+# create a function which tells you which one is the greatest number from above inputs.
+
+
+def greater(a,b):
+    if a > b:
+        return a
+    return b
+    # else:
+    #     return b
+
+first_num = int(input("enter first number : "))
+second_num = int(input("enter second number : "))
+bigger = greater(first_num, second_num)
+print(f"{bigger} is greater")
