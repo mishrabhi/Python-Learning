8ae6f1ff468bdda7b34715275bbedb009e5c9d6c
unknown <asmyselfabhishek00@gmail.com>
2021-02-26 10:28:02 -0800

    added list

--- list.py
+++ list.py
@@ -14,4 +14,13 @@ mixed = [1, 2, 3, 'Four', 'Five', 2.4, None, True]
 print(mixed)
 print(mixed[4])   #you can use indexing to print particular string or num
 mixed[2] = 'Two'  
-print(mixed)    #// [1, 2, 'Two', 'Four', 'Five', 2.4, None, True]  -->>you can change any content using indexing
+print(mixed)    #// [1, 2, 'Two', 'Four', 'Five', 2.4, None, True]  -->>you can change any content using indexing.
+
+
+# how to add items in our list
+# most common thing that you can do with your list and most important.
+# We use append --->>> it adds items at last of your list.
+fruits = ['grapes', 'apple']
+fruits.append('mango')
+print(fruits)    #// ['grapes', 'apple', 'mango']
+
